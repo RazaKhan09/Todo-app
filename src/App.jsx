eff033b317c82b2ddf21ff981d2b6f936192ac93
RazaKhan09 <raza0009k@gmail.com>
2025-08-11 17:04:55 +0530

    final change

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Todo from './components/Todo'
 import AddTodo from './components/AddTodo'
-import Header from './components/Header'
-import { Outlet } from 'react-router-dom'
-import Home from './components/Home'
 
 function App() {
   
 
   return (
     <>
-      <Header/>
-      <Home/>
+      <AddTodo/>
+      <Todo/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { Store } from './app/Store.js'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './components/Home.jsx'
-import Register from './components/Register.jsx'
-const router=createBrowserRouter([
-  {
-    path:"/",
-    element:<App/>
-  },
-  {
-    path:"/register",
-    element:<Register/>
-  }
-])
+
 
 createRoot(document.getElementById('root')).render(
   <Provider store={Store}>
-    <RouterProvider router={router}/>
+    <App/>
   </Provider>,
 )
